b5949a9fe321e3f297aad4f973028f572382f83f
Marek Mička <marek.micka04@gmail.com>
2026-04-09 19:54:42 +0200

    added structure of sig name encoder

--- code/modules/sig_name_display.vhd
+++ code/modules/sig_name_display.vhd
@@ -1,3 +1,32 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+entity sig_name_display is
+    port (
+        cnt : in std_logic_vector(3 downto 0);     --! Signal select counter value
+        data : out std_logic_vector(15 downto 0)   --! Data for display_driver module
+    );
+end entity sig_name_display;
+
+architecture behavioral of sig_name_display is
+begin
+
+text_encoder : process (cnt) is
+begin
+    case cnt is
+        when x"0" =>
+            data <= b"1111_1111_1111_1111";
+        when x"1" =>
+            data <= b"1111_1111_1111_1111";
+        when x"2" =>
+            data <= b"1111_1111_1111_1111";
+        when x"3" =>
+            data <= b"1111_1111_1111_1111";
+
+        -- Default case
+        when others =>
+            data <= b"0000_0000_0000_0000";
+    end case;
+end process text_encoder;
+
+end architecture behavioral;
