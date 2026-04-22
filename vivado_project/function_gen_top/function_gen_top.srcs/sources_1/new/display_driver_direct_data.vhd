b68b6c3737f5abd83dc16934d5fd8c692d0e149c
Marek Mička <marek.micka04@gmail.com>
2026-04-22 11:45:52 +0200

     expanded display driver

--- vivado_project/function_gen_top/function_gen_top.srcs/sources_1/new/display_driver_direct_data.vhd
+++ vivado_project/function_gen_top/function_gen_top.srcs/sources_1/new/display_driver_direct_data.vhd
@@ -7,9 +7,9 @@ entity display_driver_direct_data is
     Port (
         clk   : in  STD_LOGIC;
         rst   : in  STD_LOGIC;
-        data  : in  STD_LOGIC_VECTOR(27 downto 0);
+        data  : in  STD_LOGIC_VECTOR(55 downto 0);
         seg   : out STD_LOGIC_VECTOR(6 downto 0);
-        anode : out STD_LOGIC_VECTOR(3 downto 0)
+        anode : out STD_LOGIC_VECTOR(7 downto 0)
     );
 end display_driver_direct_data;
 
@@ -26,7 +26,7 @@ architecture Behavioral of display_driver_direct_data is
             ce  : out std_logic
         );
     end component clk_en;
- 
+
     -- Component declaration for binary counter
     component counter is
         generic ( G_BITS : positive );
@@ -37,12 +37,12 @@ architecture Behavioral of display_driver_direct_data is
             cnt : out std_logic_vector(G_BITS - 1 downto 0)
         );
     end component counter;
- 
+
     -- Internal signals
     signal sig_en    : std_logic;
-    signal sig_digit : std_logic_vector(1 downto 0);
+    signal sig_digit : std_logic_vector(2 downto 0);
     signal sig_seg   : std_logic_vector(6 downto 0);
-    
+
 begin
 
     ------------------------------------------------------------------------
@@ -60,7 +60,7 @@ begin
     -- N-bit counter for digit selection
     ------------------------------------------------------------------------
     counter_0 : counter
-        generic map ( G_BITS => 2 )
+        generic map ( G_BITS => 3 )
         port map (
             clk => clk,
             rst => rst,
@@ -71,10 +71,14 @@ begin
     ------------------------------------------------------------------------
     -- Digit select
     ------------------------------------------------------------------------
-    sig_seg <= data(6 downto 0) when sig_digit = "00" else
-               data(13 downto 7) when sig_digit = "01" else
-               data(20 downto 14) when sig_digit = "10" else
-               data(27 downto 21);
+    sig_seg <=  data(6 downto 0)   when sig_digit = "000" else
+                data(13 downto 7)  when sig_digit = "001" else
+                data(20 downto 14) when sig_digit = "010" else
+                data(27 downto 21) when sig_digit = "011" else
+                data(34 downto 28) when sig_digit = "100" else
+                data(41 downto 35) when sig_digit = "101" else
+                data(48 downto 42) when sig_digit = "110" else
+                data(55 downto 49);
 
     ------------------------------------------------------------------------
     -- Anode select process
@@ -82,16 +86,24 @@ begin
     p_anode_select : process (sig_digit) is
     begin
         case sig_digit is
-            when "00" =>
-                anode <= "1110";
-            when "01" =>
-                anode <= "1101";
-            when "10" =>
-                anode <= "1011";
-            when "11" =>
-                anode <= "0111";
+            when "000" =>
+                anode <= "11111110";
+            when "001" =>
+                anode <= "11111101";
+            when "010" =>
+                anode <= "11111011";
+            when "011" =>
+                anode <= "11110111";
+            when "100" =>
+                anode <= "11101111";
+            when "101" =>
+                anode <= "11011111";
+            when "110" =>
+                anode <= "10111111";
+            when "111" =>
+                anode <= "01111111";
             when others =>
-                anode <= "1111";  -- All off
+                anode <= "11111111";  -- All off
         end case;
     end process;
 
