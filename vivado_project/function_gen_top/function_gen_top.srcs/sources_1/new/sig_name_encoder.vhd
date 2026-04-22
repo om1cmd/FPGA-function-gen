8e149cf4225618c6048e2f59923bcf9cc66b23a9
Marek Mička <marek.micka04@gmail.com>
2026-04-22 15:32:31 +0200

    fixed lagging signals in sig_name_encode

--- vivado_project/function_gen_top/function_gen_top.srcs/sources_1/new/sig_name_encoder.vhd
+++ vivado_project/function_gen_top/function_gen_top.srcs/sources_1/new/sig_name_encoder.vhd
@@ -15,44 +15,42 @@ end entity sig_name_encoder;
 
 architecture behavioral of sig_name_encoder is
 
-    signal left : std_logic_vector(27 downto 0);
-    signal right : std_logic_vector(27 downto 0);
-
 begin
+    text_encoder : process (cnt_sig, cnt_per) is
+        variable left : std_logic_vector(27 downto 0);
+        variable right : std_logic_vector(27 downto 0);
+    begin
+        case cnt_sig is
+            when b"00" =>
+                left := b"1110000_1111010_1100010_1000011"; -- troJ
+            when b"01" =>
+                left := b"1111111_1100010_1100000_1000010"; -- -obd
+            when b"10" =>
+                left := b"0011000_1001111_1110001_0001000"; -- PILA
+            when b"11" =>
+                left := b"1111111_0100100_1001111_1101001"; -- -SIn
 
-text_encoder : process (cnt_sig, cnt_per) is
-begin
-    case cnt_sig is
-        when b"00" =>
-            left <= b"1110000_1111010_1100010_1000011"; -- troJ
-        when b"01" =>
-            left <= b"1111111_1100010_1100000_1000010"; -- -obd
-        when b"10" =>
-            left <= b"0011000_1001111_1110001_0001000"; -- PILA
-        when b"11" =>
-            left <= b"1111111_0100100_1001111_1101001"; -- -SIn
+            -- Default case
+            when others =>
+                left := b"0000000_0000000_0000000_0000000";
+        end case;
 
-        -- Default case
-        when others =>
-            left <= b"0000000_0000000_0000000_0000000";
-    end case;
+        case cnt_per is
+            when b"00" =>
+                right := b"1001111_1111111_0110000_0000110"; -- 1-E3
+            when b"01" =>
+                right := b"1001111_1111111_0110000_1001100"; -- 1-E4
+            when b"10" =>
+                right := b"1001111_1111111_0110000_0100100"; -- 1-E5
+            when b"11" =>
+                right := b"1001111_1111111_0110000_0100000"; -- 1-E6
 
-    case cnt_per is
-        when b"00" =>
-            right <= b"1001111_1111111_0110000_0000110"; -- 1-E3
-        when b"01" =>
-            right <= b"1001111_1111111_0110000_1001100"; -- 1-E4
-        when b"10" =>
-            right <= b"1001111_1111111_0110000_0100100"; -- 1-E5
-        when b"11" =>
-            right <= b"1001111_1111111_0110000_0100000"; -- 1-E6
+            -- Default case
+            when others =>
+                right := b"0000000_0000000_0000000_0000000";
+        end case;
 
-        -- Default case
-        when others =>
-            right <= b"0000000_0000000_0000000_0000000";
-    end case;
-
-    data <= left & right;
+        data <= left & right;
 
 end process text_encoder;
 
